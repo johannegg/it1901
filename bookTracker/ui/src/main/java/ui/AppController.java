b89721a32edec588c1bdf7d5e1244d9da101aacd
Johanne GG <johanne.gjedrem@icloud.com>
2023-09-21 14:05:42 +0200

    make controller and change javafx version

--- bookTracker/ui/src/main/java/ui/AppController.java
+++ bookTracker/ui/src/main/java/ui/AppController.java
@@ -1,9 +1,15 @@
 package ui;
 
+//import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+import core.FileHandler;
 
 public class AppController {
     public AppController() {
@@ -19,14 +25,34 @@ public class AppController {
     @FXML
     private ListView<String> listView;
 
+    FileHandler fileHandler = new FileHandler();
+
     @FXML
     public void initialize() {
-        System.out.println("here");
+        listView.getItems().addAll(getBookList());
+        // Platform.runLater(() -> {
+        //     listView.getItems().addAll(getBookList());
+        // });
+    }
+
+    public List<String> getBookList() {
+        return fileHandler.readFromFile("/Users/johannegronliengjedrem/itp/gr2323/bookTracker/core/src/main/java/core/bookTitles.txt");
     }
 
     @FXML
-    public void handleAddBtn(){
-        
+    public void handleAddBtn() {
+        String title = titleField.getText();
+        try {
+            if (!title.isEmpty()) {
+                fileHandler.writeToFile(title, "/Users/johannegronliengjedrem/itp/gr2323/bookTracker/core/src/main/java/core/bookTitles.txt");
+                titleField.clear();
+                listView.getItems().clear();
+                listView.getItems().addAll(getBookList());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
