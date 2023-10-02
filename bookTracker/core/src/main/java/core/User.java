e85d21a309d857cb4e0f527439583ac60dd68e7e
Johanne GG <johanne.gjedrem@icloud.com>
2023-10-02 15:33:29 +0200

    create a user class

---
+++ bookTracker/core/src/main/java/core/User.java
@@ -0,0 +1,62 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class User {
+    private String email;
+    private String username;
+    private String password;
+
+    public User(String email, String username, String password) {
+        setEmail(email);
+        setUsername(username);
+        setPassword(password);
+
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("Password requires at least 8 characters");
+        }
+        int numberOfDigits = 0;
+            char[] chars = password.toCharArray();
+            for (char c : chars) {
+                if (Character.isDigit(c)){
+                    numberOfDigits ++;
+                }
+            }
+        if (numberOfDigits < 1 || numberOfDigits == password.length()){
+            throw new IllegalArgumentException("You need at least one character or one digit");
+        }
+    }
+
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static void main(String[] args) {
+        User u1 = new User("johanne@ntnu.no", "johannegg", "1234567l");
+        System.out.println(u1.getPassword());
+    }
+
+}
