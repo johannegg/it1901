3fbb3b7a4082f9fc412553a6129ca74f3c86ed83
Camilla Huynh <camilhuy@stud.ntnu.no>
2023-10-05 10:42:55 +0200

    startpage layout

---
+++ bookTracker/ui/src/main/java/ui/StartpageController.java
@@ -0,0 +1,56 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class StartpageController {
+    
+    private List<String> imageSrcPop = new ArrayList<>(Arrays.asList("gilmore", "heller", "kawaguchi", "mellors", "moshfegh", "rooney"));
+
+    @FXML
+    private ImageView img1; 
+    
+    @FXML
+    private ImageView img2; 
+
+    @FXML
+    private ImageView img3; 
+
+    @FXML
+    private ImageView img4; 
+
+    @FXML
+    private ImageView img5;
+    
+    @FXML
+    private ImageView img6;
+
+    @FXML
+    private HBox PopHBox;
+
+
+    public void initialize() {
+
+        for (String img : imageSrcPop) {
+            ImageView imageView = new ImageView();
+            Image image = new Image("File:ui/src/main/resources/ui/BookImages/" + img + ".jpg");
+            imageView.setImage(image);
+
+            PopHBox.getChildren().add(imageView);
+        }
+    }
+    
+
+
+
+    
+
+    
+
+}
