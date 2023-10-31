10826baf9daacfb5aebc6cf8f885d4b78de8b771
Johanne GG <johanne.gjedrem@icloud.com>
2023-10-31 13:31:46 +0100

    set up REST-API structure
    Co-authored-by: @annikeqs

---
+++ bookTracker/restserver/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module bookTracker.restserver{
+    requires com.fasterxml.jackson.databind;
+
+    requires spring.web;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+
+    requires calc.core;
+
+    opens bookTracker.restserver to spring.beans, spring.context, spring.web;
+}
