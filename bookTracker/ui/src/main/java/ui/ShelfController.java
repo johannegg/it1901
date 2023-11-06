09b4c2414395200c2217b842d1ebcc378c36944e
Suzanne <suzann@stud.ntnu.no>
2023-11-06 11:09:28 +0100

    add books with author and title and scroll through bookshelf
    Co-authored-by: @camilhuy

--- bookTracker/ui/src/main/java/ui/ShelfController.java
+++ bookTracker/ui/src/main/java/ui/ShelfController.java
@@ -1,13 +1,31 @@
 package ui;
-import java.io.IOException;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import core.User;
+import javafx.scene.effect.DropShadow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ShelfController {
@@ -19,6 +37,91 @@ public class ShelfController {
 
     @FXML
     private Button homePageButton;
+    
+    @FXML
+    private ScrollBar scroll;
+    
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    private User user; 
+    private TilePane shelfTilePane;
+    private double lastX = 0;
+
+    private List<String> shelf = new ArrayList<>(
+            Arrays.asList("gilmore", "heller", "kawaguchi", "mellors", "moshfegh", "rooney", "sittenfeld", "patchett",
+                    "keane", "cauley", "sinclair", "verghese", "chambers", "kawakami", "rowley"));
+
+     public void initialize() {
+        shelfTilePane = createShelfTilePane();
+        scrollPane.setContent(shelfTilePane);
+
+        shelfTilePane.setOnMousePressed(event -> lastX = event.getSceneX());
+        shelfTilePane.setOnMouseDragged(event -> {
+            double deltaX = event.getSceneX() - lastX;
+            double newX = shelfTilePane.getLayoutX() + deltaX;
+            shelfTilePane.setLayoutX(newX);
+            lastX = event.getSceneX();
+        });
+
+    }
+
+    private TilePane createShelfTilePane() {
+        TilePane tilePane = new TilePane();
+        tilePane.setVgap(70); // Adjust the vertical gap as needed
+        tilePane.setHgap(50);
+        tilePane.setPrefColumns(4); // Display 5 books in a row
+        tilePane.setPrefWidth(1000); // Set the width of the TilePane
+
+        for (String book : shelf) {
+            //ImageView imageView = createBookImageView();
+            Node bookInfoView = createBookInfoView(book);
+            tilePane.getChildren().add(bookInfoView);
+        }
+
+        return tilePane;
+    }
+
+    private Node createBookInfoView(String book){
+        ImageView imageView = createBookImageView();
+        Label titleLabel = new Label("Book Title");
+        Label authorLabel = new Label("Author");
+
+        titleLabel.setFont(new Font(16));
+        titleLabel.setStyle("-fx-font-weight: bold"); 
+        authorLabel.setFont(new Font(14));
+
+        VBox labelContainer = new VBox();
+        labelContainer.getChildren().addAll(titleLabel, authorLabel);
+        labelContainer.setAlignment(Pos.CENTER);
+
+        StackPane bookInfoView = new StackPane();
+        bookInfoView.getChildren().addAll(imageView,labelContainer);
+        //bookInfoView.setAlignment(Pos.CENTER);
+
+        Insets margin = new Insets(0,0,0,25);
+        StackPane.setMargin(labelContainer, margin);
+
+        return bookInfoView;
+    }
+
+    private ImageView createBookImageView() {
+        ImageView imageView = new ImageView();
+        Image image = new Image(getClass().getResourceAsStream("/ui/BookImages/bookDefault.png"));
+        imageView.setImage(image);
+        imageView.setFitWidth(180); // Adjust the width as needed
+        imageView.setPreserveRatio(true);
+        // You can add event handling here if needed
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5);
+        dropShadow.setOffsetX(3);
+        dropShadow.setOffsetY(3);
+        imageView.setEffect(dropShadow);
+        return imageView;
+    }
 
     public void handleProfileButton(ActionEvent event) throws IOException {
         changeScene("/ui/Profile.fxml", event);
@@ -33,7 +136,7 @@ public class ShelfController {
     }
 
     /**
-     * Changes the scne to the given file path
+     * Changes the scene to the given file path
      * 
      * @param filePath the fxml file to change to
      * @param event    the mouse click
