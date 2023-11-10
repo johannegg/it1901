de8caa8ce2aaa361557eb25119b5b8eeb8f0c7ae
Anniken Strand <annikenqs@gmail.com>
2023-11-10 20:56:00 +0100

    fix UsersTest
    Co-authored-by: @johanggj

--- bookTracker/core/src/test/java/core/UsersTest.java
+++ bookTracker/core/src/test/java/core/UsersTest.java
@@ -1,13 +1,17 @@
 package core;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +22,6 @@ public class UsersTest {
     private Users users;
     private User user1;
     private User user2;
-    private User testUser;
 
     /**
      * Method for setting up the test correctly before running each test.
@@ -29,42 +32,31 @@ public class UsersTest {
         user1 = new User("suzanne@ntnu.no", "suzanne123", "jegerkul123");
         user2 = new User("johanne@ntnu.no", "johanne123", "jegerkul123");
 
-        users.addUserForTest(user1); //A method created for testing so that it does not actually create new users
+        users.addUserForTest(user1); // A method created for testing so that it does not actually create new users
         users.addUserForTest(user2);
-        
+
     }
 
     /**
      * Tests addUser()
      */
+
     @Test
     public void testAddUser() throws IOException {
-        List<User> result = new ArrayList<>();
-        result.add(user1);
-        result.add(user2);
-        assertArrayEquals(result.toArray(), users.getUsers().toArray());
-    }
-
-    /**
-     * Tests tat AddUser() throws an IllegalArgumemtException if username already exists
-     * @throws IOException if username already exists
-     */
-    @Test
-    public void testFailsWhenAddUser() throws IOException {
-        testUser = new User("test@ntnu.no", "test", "test1234");
-        Throwable exception =  Assertions.assertThrows(IllegalArgumentException.class, 
-        () -> users.addUser(testUser));
-        assertEquals("Username already exists", exception.getMessage());
+        User newUser = new User("new@ntnu.no", "newUser", "newPassword123");
+        users.addUser(newUser);
+        assertEquals(newUser, users.getUser("newUser"));
     }
 
     /**
      * Tests getUser()
+     * 
      * @throws IOException if username already exists
      */
     @Test
-    public void testGetUser() throws IOException {
-        assertEquals(user1, users.getUser("suzanne@ntnu.no"));
-        assertEquals(null, users.getUser("tull@ntnu.no"));
+    public void testGetUser() {
+        assertEquals(user1, users.getUser("suzanne123"));
+        assertEquals(null, users.getUser("tull"));
     }
 
     /**
@@ -76,5 +68,21 @@ public class UsersTest {
         List<User> result = new ArrayList<>();
         result.add(user1);
         assertArrayEquals(result.toArray(), users.getUsers().toArray());
+    }
+
+    @Test
+    public void testCheckUsername() throws IOException {
+        assertThrows(IllegalArgumentException.class, () -> users.checkUsername("suzanne123"));
+        assertDoesNotThrow(() -> users.checkUsername("ukjent"));
+    }
+
+    @Test
+    public void testIterator() {
+        Iterator<User> iterator = users.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(user1, iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(user2, iterator.next());
+        assertFalse(iterator.hasNext());
     }
 }
