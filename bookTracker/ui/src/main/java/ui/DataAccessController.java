71d7fa89838bbb19afc5e47a7b3fe24748ee69d2
Camilla Huynh <camilhuy@stud.ntnu.no>
2023-11-14 19:35:01 +0100

    create and fix data access classes
    Co-authored-by: @annikeqs

---
+++ bookTracker/ui/src/main/java/ui/DataAccessController.java
@@ -0,0 +1,24 @@
+package ui;
+
+abstract class DataAccessController {
+
+    private DataAccess dataAccess = new RemoteDataAccess();
+
+    /**
+     * Sets data access instance.
+     *
+     * @param dataAccess Direct- or RemoteDataAccess instance
+     */
+    public void setDataAccess(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    /**
+     * Gets the instance of DataAccess.
+     *
+     * @return DataAccess
+     */
+    public DataAccess getDataAccess() {
+        return this.dataAccess;
+    }
+}
