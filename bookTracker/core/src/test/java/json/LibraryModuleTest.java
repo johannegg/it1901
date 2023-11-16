98cb2d020a98ef767204be8647c42c71598123d5
Anniken Strand <annikenqs@gmail.com>
2023-11-16 03:21:00 +0100

    fix javadoc for json test classes
    Co-authored-by: @camilhuy

--- bookTracker/core/src/test/java/json/LibraryModuleTest.java
+++ bookTracker/core/src/test/java/json/LibraryModuleTest.java
@@ -13,41 +13,61 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.io.IOException;
+
+/**
+ * Test class for testing the LibraryModule class and its methods.
+ */
 public class LibraryModuleTest {
 
     private ObjectMapper objectMapper;
     private BookShelf bookShelf;
     private Book book;
 
+    /**
+     * Method for setting up the test evironment before running each tests.
+     */
     @BeforeEach
     public void setUp() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new LibraryModule());
-
         book = new Book();
         book.setTitle("Harry Potter");
         book.setAuthor("Rowling");
         book.setPages("900");
         book.setBookId("12345");
         book.setDescription("Bokbeskrivelse");
-
         bookShelf = new BookShelf();
         bookShelf.addBook(book);
     }
 
+    /**
+     * Method that test serialization of Book object.
+     *
+     * @throws JsonProcessingException if JSON processing fails.
+     */
     @Test
     public void testBookSerialization() throws JsonProcessingException {
         String serializedBook = objectMapper.writeValueAsString(book);
-
         assertNotNull(serializedBook);
     }
 
+    /**
+     * Method that test serialization of BookShelf objet.
+     *
+     * @throws JsonProcessingException if JSON processing fails.
+     */
     @Test
     public void testBookShelfSerialization() throws JsonProcessingException {
         String serializedBookShelf = objectMapper.writeValueAsString(bookShelf);
         assertNotNull(serializedBookShelf);
     }
 
+    /**
+     * Method that test deserialization of Book objet.
+     *
+     * @throws JsonProcessingException if JSON processing fails.
+     */
     @Test
     public void testBookDeserialization() throws JsonProcessingException {
         String jsonBook = objectMapper.writeValueAsString(book);
@@ -62,8 +82,13 @@ public class LibraryModuleTest {
 
     }
 
+    /**
+     * Tests that deserializing a null JSON node results in a null object.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
     @Test
-    void deserializeShouldReturnNullForNullNode() throws Exception {
+    void deserializeShouldReturnNullForNullNode() throws IOException {
         BookDeserializer bookDeserializer = new BookDeserializer();
         JsonNode nullNode = null;
         Book result1 = bookDeserializer.deserialize(nullNode);
--- bookTracker/core/src/test/java/json/UsersModuleTest.java
+++ bookTracker/core/src/test/java/json/UsersModuleTest.java
@@ -18,91 +18,100 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
+/**
+ * Test class for testing the UsersModule class and its methods.
+ */
 public class UsersModuleTest {
 
-    private ObjectMapper objectMapper;
-    private Users users;
-    private User user1, user2;
+        private ObjectMapper objectMapper;
+        private Users users;
+        private User user1, user2;
 
-    @BeforeEach
-    public void setUp() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new UsersModule());
+        /**
+         * Method for setting up the test evironment before running each tests.
+         */
+        @BeforeEach
+        public void setUp() {
+                objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new UsersModule());
 
-        BookShelf bookShelf = new BookShelf();
-        Book book = new Book();
-        book.setTitle("Harry Potter");
-        book.setAuthor("Johanne");
-        book.setPages("123");
-        book.setBookId("10");
-        book.setDescription("beskrivelse av bok");
-        bookShelf.addBook(book);
+                BookShelf bookShelf = new BookShelf();
+                Book book = new Book();
+                book.setTitle("Harry Potter");
+                book.setAuthor("Johanne");
+                book.setPages("123");
+                book.setBookId("10");
+                book.setDescription("beskrivelse av bok");
+                bookShelf.addBook(book);
 
-        user1 = new User();
-        user1.setEmail("camilla@ntnu.no");
-        user1.setUsername("camilla");
-        user1.setPassword("passord123");
-        user1.setBookShelf(bookShelf);
-        user1.setLoggedIn(true);
+                user1 = new User();
+                user1.setEmail("camilla@ntnu.no");
+                user1.setUsername("camilla");
+                user1.setPassword("passord123");
+                user1.setBookShelf(bookShelf);
+                user1.setLoggedIn(true);
 
-        user2 = new User();
-        user2.setEmail("suzanne@ntnu.no");
-        user2.setUsername("suzanne");
-        user2.setPassword("passord321");
-        user2.setBookShelf(bookShelf);
-        user2.setLoggedIn(false);
+                user2 = new User();
+                user2.setEmail("suzanne@ntnu.no");
+                user2.setUsername("suzanne");
+                user2.setPassword("passord321");
+                user2.setBookShelf(bookShelf);
+                user2.setLoggedIn(false);
 
-        users = new Users();
-        users.addUser(user1);
-        users.addUser(user2);
-    }
-
-    @Test
-    public void testUsersSerializationAndDeserialization() throws JsonProcessingException {
-
-        String serializedUsers = objectMapper.writeValueAsString(users);
-        assertNotNull(serializedUsers, "Serialized users should not be null");
-
-        Users deserializedUsers = objectMapper.readValue(serializedUsers, Users.class);
-        assertNotNull(deserializedUsers, "Deserialized users should not be null");
-
-        assertEquals(users.getUsers().size(), deserializedUsers.getUsers().size(),
-                "The number of users should be the same after deserialization");
-
-        for (int i = 0; i < users.getUsers().size(); i++) {
-            User originalUser = users.getUsers().get(i);
-            User deserializedUser = deserializedUsers.getUsers().get(i);
-
-            assertEquals(originalUser.getUsername(), deserializedUser.getUsername(),
-                    "Usernames should be the same after deserialization");
-            assertEquals(originalUser.getEmail(), deserializedUser.getEmail(),
-                    "Emails should be the same after deserialization");
-            assertEquals(originalUser.getPassword(), deserializedUser.getPassword(),
-                    "Passwords should be the same after deserialization");
-            assertEquals(originalUser.isLoggedIn(), deserializedUser.isLoggedIn(),
-                    "LoggedIn status should be the same after deserialization");
-
-            assertNotNull(deserializedUser.getBookShelf(),
-                    "BookShelf should not be null after deserialization");
-            assertEquals(originalUser.getBookShelf().getBooks().size(),
-                    deserializedUser.getBookShelf().getBooks().size(),
-                    "BookShelf size should be the same after deserialization");
+                users = new Users();
+                users.addUser(user1);
+                users.addUser(user2);
         }
 
-    }
+        /**
+         * Method that tests serialization and deserialization of Users objects.
+         *
+         * @throws JsonProcessingException if JSON processing fails.
+         * 
+         */
+        @Test
+        public void testUsersSerializationAndDeserialization() throws JsonProcessingException {
 
-    @Test
-    public void testUserDeserializationFromJsonString() throws IOException {
-        String json = "{\"username\":\"camilla\",\"email\":\"camilla@ntnu.no\",\"password\":\"passord123\",\"loggedIn\":true,\"bookShelf\":{\"books\":[]}}";
-        JsonParser parser = objectMapper.getFactory().createParser(json);
-        DeserializationContext ctxt = objectMapper.getDeserializationContext();
-        User user = new UserDeserializer().deserialize(parser, ctxt);
+                String serializedUsers = objectMapper.writeValueAsString(users);
+                assertNotNull(serializedUsers);
+                Users deserializedUsers = objectMapper.readValue(serializedUsers, Users.class);
+                assertNotNull(deserializedUsers);
+                assertEquals(users.getUsers().size(), deserializedUsers.getUsers().size());
 
-        assertNotNull(user, "User deserialized from JSON string should not be null");
-        assertEquals("camilla", user.getUsername(), "Usernames should match");
-        assertEquals("camilla@ntnu.no", user.getEmail(), "Emails should match");
-        assertEquals("passord123", user.getPassword(), "Passwords should match");
-        assertTrue(user.isLoggedIn(), "User should be logged in");
-    }
+                for (int i = 0; i < users.getUsers().size(); i++) {
+                        User originalUser = users.getUsers().get(i);
+                        User deserializedUser = deserializedUsers.getUsers().get(i);
+
+                        assertEquals(originalUser.getUsername(), deserializedUser.getUsername());
+                        assertEquals(originalUser.getEmail(), deserializedUser.getEmail());
+                        assertEquals(originalUser.getPassword(), deserializedUser.getPassword());
+                        assertEquals(originalUser.isLoggedIn(), deserializedUser.isLoggedIn());
+
+                        assertNotNull(deserializedUser.getBookShelf());
+                        assertEquals(originalUser.getBookShelf().getBooks().size(),
+                                        deserializedUser.getBookShelf().getBooks().size());
+                }
+
+        }
+
+        /**
+         * Method that tests the deserialization of a User object from a JSON string.
+         *
+         * @throws IOException if an I/O error occurs.
+         * 
+         */
+        @Test
+        public void testUserDeserializationFromJsonString() throws IOException {
+                String json = "{\"username\":\"camilla\",\"email\":\"camilla@ntnu.no\",\"password\":\"passord123\",\"loggedIn\":true,\"bookShelf\":{\"books\":[]}}";
+                JsonParser parser = objectMapper.getFactory().createParser(json);
+                DeserializationContext ctxt = objectMapper.getDeserializationContext();
+                User user = new UserDeserializer().deserialize(parser, ctxt);
+
+                assertNotNull(user);
+                assertEquals("camilla", user.getUsername());
+                assertEquals("camilla@ntnu.no", user.getEmail());
+                assertEquals("passord123", user.getPassword());
+                assertTrue(user.isLoggedIn());
+        }
 
 }
